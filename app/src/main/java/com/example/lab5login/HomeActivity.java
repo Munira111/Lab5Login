6aa38ea49c5c05b006e32d054a7b9f0fa6cb1b28
munira111 <Munira.alhammad.3@gmail.com>
2023-05-04 23:58:01 +0300

    p1

--- app/src/main/java/com/example/lab5login/HomeActivity.java
+++ app/src/main/java/com/example/lab5login/HomeActivity.java
@@ -1,14 +1,43 @@
 package com.example.lab5login;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class HomeActivity extends AppCompatActivity {
 
+    BottomNavigationView bottomNavigationView;
+    homeFragment homeFragment = new homeFragment();
+    profileFragment profileFragment = new profileFragment();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.containar,homeFragment).commit();
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected (MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.containar,homeFragment).commit();
+                        return true;
+                    case R.id.profile:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.containar,profileFragment).commit();
+                        return true;
+                }
+                return false;
+            }
+        });
+
     }
 }
